5deb74880e5c71213df80c398d01abefa51b2466
linnefromice <linnefromice@gmail.com>
2024-02-20 21:28:40 +0900

    update: mock functions & define storage

--- src/ic_todolist_backend/src/lib.rs
+++ src/ic_todolist_backend/src/lib.rs
@@ -1,4 +1,38 @@
+use std::cell::RefCell;
+use candid::CandidType;
+
+thread_local! {
+    static TODOS: RefCell<Vec<Todo>> = RefCell::new(Vec::new());
+}
+
+#[derive(CandidType)]
+struct Todo {
+    contents: String,
+    is_completed: bool,
+}
+
 #[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+fn get(_idx: u64) -> Todo {
+    Todo { contents: "Dummy".to_string(), is_completed: false }
+}
+
+#[ic_cdk::update]
+fn add(_contents: String) -> u64 {
+    0
+}
+
+#[ic_cdk::update]
+fn update_status(_is_completed: bool) {
+    // dummy
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    candid::export_service!();
+
+    #[test]
+    fn gen_candid() {
+        std::fs::write("ic_todolist_backend.did", __export_service()).unwrap();
+    }
 }
